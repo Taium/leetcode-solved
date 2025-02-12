4bf74be2bd3dae01901b43ba94014fcce2a7f054
Taium <taium44@gmail.com>
2025-02-12 12:07:43 +0600

    add two binary number

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-var plusOne = function (digits) {
-  const number = BigInt(digits.join("")) + BigInt(1);
-  return number.toString().split("").map(Number);
+var addBinary = function (a, b) {
+    const sum = BigInt(`0b${a}`) + BigInt(`0b${b}`);
+    return sum.toString(2);
 };
-
-console.log(plusOne([6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3]));
